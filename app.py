3cfa23a99d7782a7928379f3aa713dcb66c78338
unknown <fnicolegm12@outlook.com>
2026-05-07 18:29:18 -0600

    basic login and fetch user

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
 import psycopg2
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = os.environ['SECRET_KEY']
 
 def get_db_connection():
     conn = psycopg2.connect(host='localhost',
@@ -34,6 +35,31 @@ def index():
         return f"Error de conexión: {e}"
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'POST':
+
+        user_id = request.form['user_id']
+        password = request.form['password']
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        cur.execute('''
+            SELECT id, user_id, password_hash, role
+            FROM users
+            WHERE user_id = %s
+        ''', (user_id,))
+
+        user = cur.fetchone()
+
+        cur.close()
+        conn.close()
+
+        return str(user)
+
+    return render_template('login.html')
 
 
 
